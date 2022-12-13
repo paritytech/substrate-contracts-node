87a3d76c88058667d8c028b886a5e404ef9a70ea
German <german@parity.io>
2022-12-13 13:17:50 +0000

    Update to polkadot-v0.9.34 branch (#166)
    Sync the repository with Substrate's
    [`polkadot-v0.9.34`](https://github.com/paritytech/substrate/tree/polkadot-v0.9.34)
    branch.
    
    Co-authored-by: Bastian Köcher <info@kchr.de>
    Co-authored-by: Alexander Theißen <alex.theissen@me.com>

--- node/src/cli.rs
+++ node/src/cli.rs
@@ -2,10 +2,10 @@ use sc_cli::RunCmd;
 
 #[derive(Debug, clap::Parser)]
 pub struct Cli {
-	#[clap(subcommand)]
+	#[command(subcommand)]
 	pub subcommand: Option<Subcommand>,
 
-	#[clap(flatten)]
+	#[command(flatten)]
 	pub run: RunCmd,
 }
 
