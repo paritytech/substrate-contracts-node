e68e005a3af5d869659270f601b7045942b39581
Ben Greenberg <13892277+hummusonrails@users.noreply.github.com>
2023-06-05 16:40:45 +0200

    Update updateContentCommand.sh

--- .devcontainer/updateContentCommand.sh
+++ .devcontainer/updateContentCommand.sh
@@ -17,11 +17,14 @@ sudo unzip -q protoc.zip bin/protoc -d /usr/local
 sudo chmod a+x /usr/local/bin/protoc
 rm -rf protoc.zip
 
+echo "Installing Substrate Contracts Node..."
 SUBSTRATE_VERSION=$(curl -s "https://api.github.com/repos/paritytech/substrate-contracts-node/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
 curl -Lo substrate.tar.gz "https://github.com/paritytech/substrate-contracts-node/releases/download/${SUBSTRATE_VERSION}/substrate-contracts-node-linux.tar.gz"
-sudo tar -xzf substrate.tar.gz bin/substrate-contracts-node -d /usr/local
+mkdir substrate-temp
+tar -xzf substrate.tar.gz -C substrate-temp
+sudo mv substrate-temp/artifacts/substrate-contracts-node-linux/substrate-contracts-node /usr/local/bin/
 sudo chmod a+x /usr/local/bin/substrate-contracts-node
-rm -rf substrate.tar.gz
+rm -rf substrate.tar.gz substrate-temp
 
 echo "Installing cargo-contract CLI tool..."
 cargo install --force --locked cargo-contract --version 2.0.0-rc
