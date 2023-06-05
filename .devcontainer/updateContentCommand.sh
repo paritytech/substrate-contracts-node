b61343dcde7af4197a053b447c1c697f7560fef1
Ben Greenberg <13892277+hummusonrails@users.noreply.github.com>
2023-06-05 16:55:34 +0200

    Update updateContentCommand.sh

--- .devcontainer/updateContentCommand.sh
+++ .devcontainer/updateContentCommand.sh
@@ -19,7 +19,7 @@ rm -rf protoc.zip
 
 echo "Installing Substrate Contracts Node..."
 SUBSTRATE_VERSION=$(curl -s "https://api.github.com/repos/paritytech/substrate-contracts-node/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
-curl -Lo substrate.tar.gz "https://github.com/paritytech/substrate-contracts-node/releases/download/${SUBSTRATE_VERSION}/substrate-contracts-node-linux.tar.gz"
+curl -Lo substrate.tar.gz "https://github.com/paritytech/substrate-contracts-node/releases/download/v${SUBSTRATE_VERSION}/substrate-contracts-node-linux.tar.gz"
 mkdir substrate-temp
 tar -xzf substrate.tar.gz -C substrate-temp
 sudo mv substrate-temp/artifacts/substrate-contracts-node-linux/substrate-contracts-node /usr/local/bin/
