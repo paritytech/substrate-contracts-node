963e8715f94a03142334db80cccdab92ddcadfe7
Ben Greenberg <13892277+hummusonrails@users.noreply.github.com>
2023-06-05 15:40:24 +0200

    Update updateContentCommand.sh

--- .devcontainer/updateContentCommand.sh
+++ .devcontainer/updateContentCommand.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-echo "Installing Rust and WASM toolchain..."
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-rustup default stable
-rustup update
-rustup update nightly
-rustup target add wasm32-unknown-unknown
-rustup target add wasm32-unknown-unknown --toolchain nightly
+# echo "Installing Rust and WASM toolchain..."
+# curl https://sh.rustup.rs -sSf | sh -s -- -y
+# rustup default stable
+# rustup update
+# rustup update nightly
+# rustup target add wasm32-unknown-unknown
+# rustup target add wasm32-unknown-unknown --toolchain nightly
 
 echo "Installing protoc..."
 sudo apt update
@@ -17,5 +17,12 @@ sudo unzip -q protoc.zip bin/protoc -d /usr/local
 sudo chmod a+x /usr/local/bin/protoc
 rm -rf protoc.zip
 
+SUBSTRATE_VERSION=$(curl -s "https://api.github.com/repos/paritytech/substrate-contracts-node/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+curl -Lo substrate.tar.gz "https://github.com/paritytech/substrate-contracts-node/releases/download/${SUBSTRATE_VERSION}/substrate-contracts-node-linux.tar.gz"
+tar -xzf substrate.tar.gz -d /usr/local/bin
+sudo chmod a+x /usr/local/bin/substrate-contracts-node
+rm -rf substrate.tar.gz
+
 echo "Installing cargo-contract CLI tool..."
 cargo install --force --locked cargo-contract --version 2.0.0-rc
+
