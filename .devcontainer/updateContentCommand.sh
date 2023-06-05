6dcd84eae4b3abea2fe1d582534b8d50ae87af52
Ben Greenberg <13892277+hummusonrails@users.noreply.github.com>
2023-06-05 12:21:39 +0200

    Remove the build of the node from post-create
    Since it's now fetched from the Docker image this is not needed

--- .devcontainer/updateContentCommand.sh
+++ .devcontainer/updateContentCommand.sh
@@ -17,8 +17,5 @@ sudo unzip -q protoc.zip bin/protoc -d /usr/local
 sudo chmod a+x /usr/local/bin/protoc
 rm -rf protoc.zip
 
-echo "Building Substrate Node Template..."
-cargo build --release --locked
-
 echo "Installing cargo-contract CLI tool..."
 cargo install --force --locked cargo-contract --version 2.0.0-rc
