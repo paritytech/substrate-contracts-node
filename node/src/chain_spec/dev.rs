f0c897c20527c106f45797be6a40cc90a136ccb7
pgherveou <pgherveou@gmail.com>
2024-01-16 15:42:05 +0100

    test

--- node/src/chain_spec/dev.rs
+++ node/src/chain_spec/dev.rs
@@ -1,7 +1,5 @@
 use contracts_node_runtime::{AccountId, RuntimeGenesisConfig, Signature, WASM_BINARY};
 use sc_service::ChainType;
-use sp_consensus_aura::sr25519::AuthorityId as AuraId;
-use sp_consensus_grandpa::AuthorityId as GrandpaId;
 use sp_core::{sr25519, Pair, Public};
 use sp_runtime::traits::{IdentifyAccount, Verify};
 
