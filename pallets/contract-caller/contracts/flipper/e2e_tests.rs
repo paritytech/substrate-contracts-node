e2eaf03022ecafd1032dd415f611b2139c0c4408
Andrew Jones <ascjones@gmail.com>
2024-03-15 12:38:04 +0000

    Failing e2e test

--- pallets/contract-caller/contracts/flipper/e2e_tests.rs
+++ pallets/contract-caller/contracts/flipper/e2e_tests.rs
@@ -1,11 +1,13 @@
 use super::flipper::*;
+use flipper_traits::Flip;
 use ink_e2e::{ChainBackend, ContractsBackend};
 
 type E2EResult<T> = Result<T, Box<dyn std::error::Error>>;
 
 #[ink_e2e::test]
 async fn instantiate_and_get<Client: E2EBackend>(mut client: Client) -> E2EResult<()> {
-	let mut constructor = FlipperRef::new(false);
+	let initial_value = false;
+	let mut constructor = FlipperRef::new(initial_value);
 
 	let contract = client
 		.instantiate("flipper", &ink_e2e::alice(), &mut constructor)
@@ -21,15 +23,21 @@ async fn instantiate_and_get<Client: E2EBackend>(mut client: Client) -> E2EResul
 			"contract_call_flip",
 			vec![
 				scale_value::Value::from_bytes(contract.account_id),
-				scale_value::serde::to_value(
-					frame_support::weights::Weight::from_parts(1_000_000_000, 0),
-				).unwrap(),
+				scale_value::serde::to_value(frame_support::weights::Weight::from_parts(
+					1_000_000_000,
+					0,
+				))
+				.unwrap(),
 			],
 		)
 		.await
 		.expect("runtime call failed");
 
-	println!("contract: {:?}", contract.account_id);
+	// now check that the flip was executed via the pallet
+	let get_message = contract.call_builder::<Flipper>().get();
+	let get_result = client.call(&ink_e2e::alice(), &get_message).dry_run().await?;
+
+	assert_eq!(get_result.return_value(), !initial_value);
 
 	Ok(())
 }
